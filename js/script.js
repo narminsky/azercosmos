319971dffbe9f2b5faa85bf6f92237cf8f41cf04
Narmin Sky <82314949+narminsky@users.noreply.github.com>
2022-10-30 01:05:59 +0400

    header, carousels, products all working, responsive and footer remain

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// header ul section
+const news = document.querySelector('.news-ul');
+
+$('.main-ul').mouseenter(function (e) {
+  if (e.currentTarget == $(this)[0]) {
+    $(this).children().eq(0).removeClass('d-none');
+    $(this).children().eq(0).animate({
+      right: `100%`,
+      opacity: 1,
+    });
+  }
+});
+
+$('.main-ul').mouseleave(function (e) {
+  if (e.currentTarget == $(this)[0]) {
+    $(this).children().eq(0).animate({
+      right: `0%`,
+      opacity: 0,
+    })
+    $(this).children().eq(0).addClass('d-none');
+  }
+});
+
+// products sections
 const productsArr = [{
   h1: 'Video',
   p: `We deliver your content`,
@@ -28,24 +52,26 @@ fetch(`
 https://newsapi.org/v2/everything?q=apple&from=2022-10-27&to=2022-10-27&sortBy=popularity&apiKey=2b8e326d6ced4217a494b674d251bb37`)
   .then(reponse => reponse.json())
   .then(data => {
-    console.log(data.articles[0]);
     for (let i = 0; i < 4; i++) {
       const text = `
       <div class="col-lg-3">
-                    <div class="card">
-                      <img class="w-100" src="${data.articles[i].urlToImage}" alt="">
-                      <div class="card-body">
-                            <h1 class="card-title">${data.articles[i].title}</h1>
-                            <p class="card-text">${data.articles[i].content}</p>
-                            <div class="btn-row">
-                                <a href=${data.articles[i].url} target='_blank' class="btn btn-outline-white btn-lg">Read more</a>
-                            </div>
-                      </div>
-                    </div>
+        <div class="card">
+          <img class="w-100" src="${data.articles[i].urlToImage}" alt="">
+          <div class="card-body">
+                <h1 class="card-title">${data.articles[i].title}</h1>
+                <p class="card-text">${data.articles[i].content}</p>
+                <div class="btn-row">
+                    <button class="btn btn-outline-white btn-lg" url='${data.articles[i].url}'>Read more</button>
+                </div>
+          </div>
+        </div>
         </div>
       `;
       products.innerHTML += text;
     }
+    $('.card-body button').click(function () {
+      window.open(`${$(this).attr('url')}`);
+    });
   });
 
 // for (let i = 0; i < 4; i++) {
@@ -77,21 +103,22 @@ let carouselItems;
 let width;
 
 // fetch API for news
-fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-09-28&sortBy=publishedAt&apiKey=2b8e326d6ced4217a494b674d251bb37`)
+fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-09-29&sortBy=publishedAt&apiKey=2b8e326d6ced4217a494b674d251bb37`)
   .then(response => response.json())
   .then(data => {
-    console.log(data.articles[0]);
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 10; i++) {
       const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       const d = new Date(data.articles[i].publishedAt);
       const text = `
-      <div class="col-lg-4">
-                            <div class="box">
-                                <img class="w-100" src="${data.articles[i].urlToImage}" alt="">
-                                <p class="published-date">${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}</p>
-                                <p class="news-content">${data.articles[i].title}</p>
-                            </div>
-                        </div>
+      <div class="col-lg-4 col-md-6 col-sm-12">
+        <div class="box">
+          <img class="w-100" src="${data.articles[i].urlToImage}" alt="">
+          <div class='news-content'>
+            <p class="published-date">${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}</p>
+            <p class="news-title">${data.articles[i].title}</p>
+          </div>
+        </div>
+      </div>
       `;
       carouselItemsContainer.innerHTML += text;
     }
@@ -106,7 +133,14 @@ fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-09-28&sortBy=publishe
 
 // setting
 let transformMeasure = 0;
-let maxTransform = 3;
+let maxTransform;
+if (screen.availWidth >= 992) {
+  maxTransform = 3;
+} else if (screen.availWidth >= 768) {
+  maxTransform = 2;
+} else if (screen.availWidth >= 576) {
+  maxTransform = 1;
+}
 
 prevSlide.addEventListener('click', function () {
   if (transformMeasure != 0)
